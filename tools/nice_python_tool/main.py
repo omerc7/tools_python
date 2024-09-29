7eb4aba56fad9c36d67119391609e12042d303fa
omerc7 <omerc7@gmail.com>
2024-09-29 11:29:02 +0300

    weather tool

---
+++ tools/nice_python_tool/main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_weather():
+    return requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=32.0809&longitude=34.7806&hourly=temperature_2m"
+    )
+
+
+def main():
+    response = get_weather()
+    print(response.json())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/nice_python_tool/tool_def.py
@@ -0,0 +1,38 @@
+import inspect
+from pathlib import Path
+
+
+from kubiya_sdk.tools.models import Tool, Arg, FileSpec
+from kubiya_sdk.tools.registry import tool_registry
+
+from . import main
+
+hello_tool = Tool(
+    name="get_weather",
+    type="docker",
+    image="python:3.11",
+    description="Prints the weather",
+    # args=[Arg(name="name", description="Name to greet", required=True)],
+    content="""
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+uv venv
+source venv/bin/activate
+
+uv pip install -r /tmp/requirements.txt
+
+python /tmp/main.py
+""",
+    with_files=[
+        FileSpec(
+            destination="/tmp/requirements.text",
+            content=Path("./requirements.txt").read_text(),
+        ),
+        FileSpec(
+            destination="/tmp/main.py",
+            content=inspect.getsource(main),
+        ),
+    ],
+)
+
+tool_registry.register("hello", hello_tool)
